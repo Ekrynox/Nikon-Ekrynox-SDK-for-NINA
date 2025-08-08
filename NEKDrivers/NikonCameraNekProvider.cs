e4a951de3b1a9694b16414652b84a632b517a6e2
Ekrynox <lucas123.a17@gmail.com>
2025-08-09 00:16:59 +0200

    List Nikon Camera with NEK

---
+++ NEKDrivers/NikonCameraNekProvider.cs
@@ -0,0 +1,38 @@
+﻿using NINA.Core.Utility;
+using NINA.Equipment.Interfaces;
+using NINA.Equipment.Interfaces.ViewModel;
+using NINA.Profile.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace LucasAlias.NINA.NEK.NEKDrivers {
+	[Export(typeof(IEquipmentProvider))]
+	public class NikonCameraNekProvider : IEquipmentProvider<ICamera> {
+		private IProfileService profileService;
+
+		[ImportingConstructor]
+		public NikonCameraNekProvider(IProfileService profileService) {
+			this.profileService = profileService;
+		}
+
+		public string Name => "Nikon";
+
+		public IList<ICamera> GetEquipment() {
+			var devices = new List<ICamera>();
+
+			var nekCameraList = NEKCS.NikonCamera.listNikonCameras();
+			foreach (var camera in nekCameraList) {
+			    devices.Add(new NikonCameraNek(camera.Key, camera.Value));
+			}
+
+			return devices;
+		}
+	}
+}
