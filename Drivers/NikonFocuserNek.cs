3c212063286f8e10c972f4542f7f2876a6df97f4
Ekrynox <lucas123.a17@gmail.com>
2026-02-06 23:56:57 +0100

    Generate a Lens Name

--- Drivers/NikonFocuserNek.cs
+++ Drivers/NikonFocuserNek.cs
@@ -63,8 +63,8 @@ namespace LucasAlias.NINA.NEK.Drivers {
 
             public bool HasSetupDialog { get => false; }
             public string Id { get => "NEKLF"; }
-            public string Name { get => cameraNek != null ? cameraNek.Name + " Lens" : "NEK Lens Focuser"; }
-            public string DisplayName { get => "Nikon Lens Focuser (NEK)"; }
+            public string Name { get => cameraNek != null ? "Nikon " + cameraNek.LensName : "NEK Lens Focuser"; }
+            public string DisplayName { get => cameraNek != null ? "Nikon " + cameraNek.LensName + " (NEK)" : "Nikon Lens Focuser (NEK)"; }
             public string Category { get => "Nikon"; }
             public bool Connected { get => this._isConnected && this.cameraNek != null; }
             public string Description { get => "The lens focus driver of your Nikon Camera !"; }
